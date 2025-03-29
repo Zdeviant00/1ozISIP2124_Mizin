5cb684667cbd2335e6b5f7f6bd7ac1bbbf94e9ed
249829 <249829@KB37-120-C03>
2025-03-29 11:40:15 +0300

    часть 1 ПР4 - не доделана

---
+++ 1ozISIP2124_Mizin/Pages/User_managment.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1ozISIP2124_Mizin.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для User_managment.xaml
+    /// </summary>
+    public partial class User_managment : Page
+    {
+        public User_managment()
+        {
+            InitializeComponent();
+        }
+    }
+}
